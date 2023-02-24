161b81d454b82b2e2bb01b9865297071d49c916c
B101108093王創進 <sun96813@example.com>
2023-02-24 21:09:30 +0800

    message

--- hw01.py
+++ hw01.py
@@ -18,4 +18,31 @@ for i in range(len(binary_list)-1,-1,-1):
         binary_list[len(binary_list) - (i + 1)] = 1
         binary_ori = binary_ori - 2**i
 print("This is your binary number:",*binary_list)
-#十六進位轉換
+#二進位轉換十六進位
+length = len(binary_list)
+hexadecimal_list = [0, 0]
+
+hexadecimal_1 = 0
+hexadecimal_2 = 0
+total_1 = 0
+total_2 = 0
+#將經過二進位轉換後的list切分成前後兩段
+binary_list_1 =  binary_list[0:length//2]
+binary_list_2 =  binary_list[length//2:len(binary_list)]
+
+#第一段數字加總
+binary_list_1.reverse()
+for i in range(len(binary_list_1)):
+    total_1 += binary_list_1[i] * (2**i)
+print(total_1)
+#第二段數字加總
+binary_list_2.reverse()
+for i in range(len(binary_list_2)):
+    total_2 += binary_list_2[i] * (2**i)
+print(total_2) 
+
+#將加總數字對應到十六進位的轉換表
+hexadecimal_dict = {'1': 1,'2': 2,'3': 3,'4': 4,'5': 5,'6': 6,'7': 7,'8': 8,'9': 9,
+                    '10': 'A', '11': 'B', '12':'C', '13':'D', '14': 'E', '15': 'F', '16': 'G'}
+#輸出十六進位答案
+print(str(hexadecimal_dict[str(total_1)])+str(hexadecimal_dict[str(total_2)]))
