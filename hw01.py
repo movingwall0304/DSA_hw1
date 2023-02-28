a649cf9516c5f878ff96f72d22e8c8693d6d727d
B101108093王創進 <sun96813@example.com>
2023-02-28 21:10:26 +0800

    message

--- hw01.py
+++ hw01.py
@@ -34,15 +34,13 @@ binary_list_2 =  binary_list[length//2:len(binary_list)]
 binary_list_1.reverse()
 for i in range(len(binary_list_1)):
     total_1 += binary_list_1[i] * (2**i)
-print(total_1)
 #第二段數字加總
 binary_list_2.reverse()
 for i in range(len(binary_list_2)):
     total_2 += binary_list_2[i] * (2**i)
-print(total_2) 
 
 #將加總數字對應到十六進位的轉換表
 hexadecimal_dict = {'1': 1,'2': 2,'3': 3,'4': 4,'5': 5,'6': 6,'7': 7,'8': 8,'9': 9,
                     '10': 'A', '11': 'B', '12':'C', '13':'D', '14': 'E', '15': 'F', '16': 'G'}
 #輸出十六進位答案
-print(str(hexadecimal_dict[str(total_1)])+str(hexadecimal_dict[str(total_2)]))
+print("This is your hexadecimal number:",str(hexadecimal_dict[str(total_1)])+str(hexadecimal_dict[str(total_2)]))
